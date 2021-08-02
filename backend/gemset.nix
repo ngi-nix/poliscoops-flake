e479177084e0fab8f8246c148a5245cefd906c79
Bryan Honof <bryanhonof@gmail.com>
2021-08-02 16:21:38 +0200

    Format gemset

--- backend/gemset.nix
+++ backend/gemset.nix
@@ -1,9 +1,9 @@
 {
   docsplit = {
-    groups = ["default"];
-    platforms = [];
+    groups = [ "default" ];
+    platforms = [ ];
     source = {
-      remotes = ["https://rubygems.org"];
+      remotes = [ "https://rubygems.org" ];
       sha256 = "0qx8sr7klp272zylsnf7bdq77vizxd2bc9ym2vb4mky93nb68k55";
       type = "gem";
     };
