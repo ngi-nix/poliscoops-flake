aa73df6472f321efe266fb2b657d28e10f2670da
Bryan Honof <bryanhonof@gmail.com>
2021-08-03 10:11:47 +0200

    Update the flake lock

--- flake.nix
+++ flake.nix
@@ -1,63 +1,49 @@
 {
-  inputs = {
-    nixpkgs.url = "nixpkgs";
-  };
+  inputs = { nixpkgs.url = "nixpkgs"; };
 
   outputs = { nixpkgs, self }:
     let
       supportedSystems = [ "x86_64-linux" "i686-linux" "aarch64-linux" ];
       forAllSystems' = systems: fun: nixpkgs.lib.genAttrs systems fun;
       forAllSystems = forAllSystems' supportedSystems;
-    in
-      with nixpkgs.lib;
-      {
-        overlays.poliscoops =
-          final: prev:
-          {
-            poliscoops-htdocs = final.callPackage ./htdocs {};
-            docsplit = final.callPackage ./backend/docsplit/default.nix {};
-          };
-        
-        packages = forAllSystems (system:
-          let
-            pkgs = import nixpkgs
-              { inherit system;
-                overlays = mapAttrsToList (_: id) self.overlays;
-              };
-          in
-            {
-              inherit (pkgs)
-                poliscoops-htdocs
-                docsplit;
-            }
-        );
-
-        nixosModules.poliscoops = import ./module.nix;
-
-        nixosModule = self.nixosModules.poliscoops;
-
-        nixosConfigurations =
-          { container = nixosSystem {
-              system = "x86_64-linux";
-
-              modules = [
-                self.nixosModule
-                ({ ... }:
-                  {
-                    boot.isContainer = true;
-                    nixpkgs.overlays = [ self.overlays.poliscoops ];
-                    
-                    services.poliscoops = {
-                      enable = true;
-
-                      virtualLocations =
-                        { localhost = "/"; };
-                    };
-
-                    networking.firewall.enable = false;
-                  })
-              ];
-            };
-          };
+    in with nixpkgs.lib; {
+      overlays.poliscoops = final: prev: {
+        poliscoops-htdocs = final.callPackage ./htdocs { };
+        docsplit = final.callPackage ./backend/docsplit { };
       };
+
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = mapAttrsToList (_: id) self.overlays;
+          };
+        in { inherit (pkgs) poliscoops-htdocs docsplit; });
+
+      nixosModules.poliscoops = import ./module.nix;
+
+      nixosModule = self.nixosModules.poliscoops;
+
+      nixosConfigurations = {
+        container = nixosSystem {
+          system = "x86_64-linux";
+
+          modules = [
+            self.nixosModule
+            ({ ... }: {
+              boot.isContainer = true;
+              nixpkgs.overlays = [ self.overlays.poliscoops ];
+
+              services.poliscoops = {
+                enable = true;
+
+                virtualLocations = { localhost = "/"; };
+              };
+
+              networking.firewall.enable = false;
+            })
+          ];
+        };
+      };
+    };
 }
