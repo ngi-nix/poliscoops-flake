cd48cfe40aec7e70fb8167dd14075b73877431c6
Bryan Honof <bryanhonof@gmail.com>
2021-08-03 10:10:36 +0200

    Regenerate the Gemfile.lock
    Regenerating the gemfile.lock seems to fix a runtime issue when building
    the gem trough a flake.

--- backend/docsplit/default.nix
+++ backend/docsplit/default.nix
@@ -1,18 +1,6 @@
-{ stdenv, lib, bundlerApp }:
-let
-  name = "docsplit";
-  version = "0.7.6";
-in bundlerApp {
-  pname = name;
-  exes = [ "docsplit" ];
+{ bundlerApp }:
+bundlerApp {
+  pname = "docsplit";
   gemdir = ./.;
-
-  meta = with lib; {
-    Description =
-      "A command-line utility and Ruby library for splitting apart documents into their component parts";
-    homepage = "https://github.com/documentcloud/docsplit";
-    license = licenses.lgpl3Only;
-    maintainers = [ ];
-    platforms = platform.unix;
-  };
+  exes = [ "docsplit" ];
 }
